6e447dc476629fe5afcfc81e05b461005c6ddd3a
Bruno Montebello <brunormontebello@hotmail.com>
2022-05-26 02:47:07 +0100

    Add gem Faker + seed DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Campaign.destroy_all
+
+20.times do |index|
+  Campaign.create!(
+                name: Faker::Company.name,
+                sector: Faker::Company.industry,
+                country: Faker::Address.country,
+                target_amount: Faker::Number.number(digits: 6),
+                investment_multiple: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+                )
+end
+
+p "Created #{Campaign.count} campaigns"
