9568eb3caead5be07022f24de611d0ba82a71eb7
Bruno Montebello <brunormontebello@hotmail.com>
2022-05-30 02:59:30 +0100

    Refact seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 Campaign.destroy_all
 
-20.times do |index|
+40.times do |index|
   Campaign.create!(
                 name: Faker::Company.name,
                 sector: Faker::Company.industry,
                 country: Faker::Address.country,
                 target_amount: Faker::Number.number(digits: 6),
                 investment_multiple: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+                percentage_raised: '0'
                 )
 end
 
